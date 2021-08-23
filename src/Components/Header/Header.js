fb2332a5b0324829713d04f68e6082adb288a370
SasikaAththanaya <sasikaharshana97@gmail.com>
2021-08-24 00:50:37 +0530

    Api conected

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,27 +1,29 @@
 import {AppBar,Toolbar,Typography,InputBase,Box} from "@material-ui/core";
-//import { Autocomplete } from "@react-google-maps/api";
 import SearchIcon from "@material-ui/icons/Search";
-import { Autocomplete } from "@material-ui/lab";
+import { Autocomplete } from "@react-google-maps/api";
+import useStyles from './Style';
+
 function Header() {
+    const classes =useStyles();
     return (
         <div>
             <AppBar position="static">
-                <Toolbar className="toolbar">
-                    <Typography variant="h5" className="title">
+                <Toolbar className={classes.toolbar}>
+                    <Typography variant="h5" className={classes.title}>
                         Travel Advicer
                     </Typography>
                     <Box display="flex">
-                        <Typography variant="h6" className="title">
+                        <Typography variant="h6" className={classes.title}>
                             Explore New Places
                         </Typography>
-                        <Autocomplete>
-                            <div className="search">
-                                <div className="searchIcon">
+                        {/*<Autocomplete>*/}
+                            <div className={classes.search}>
+                                <div className={classes.searchIcon}>
                                     <SearchIcon/>
                                 </div>
-                                <InputBase placeholder="search ..."/>
+                                <InputBase placeholder="search ..." classes={{root: classes.inputRoot ,input: classes.inputInput}}/>
                             </div>
-                        </Autocomplete>
+                        {/*</Autocomplete>*/}
                     </Box>
                 </Toolbar>      
             </AppBar>
